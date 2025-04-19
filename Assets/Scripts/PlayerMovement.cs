e0c593ae5f246bec8d3fe533212b89bce71175bb
Mehmet Mert Özbaykuş <mehmetozbaykus@gmail.com>
2025-04-19 10:15:36 +0300

    Crouching Added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,9 +19,40 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
 
+    //Crouch
+    public float crouchSpeed = 6f;
+    public float crouchHeight = 0.8f;
+    private float originalHeight;
+    private Vector3 originalCenter;
+    private bool isCrouching = false;
+
+    public Transform playerCamera;
+    public float crouchCameraOffset = 0.6f;
+    public float cameraSmoothSpeed = 10f;
+
+    private Vector3 cameraStandPosition;
+    private Vector3 cameraCrouchPosition;
+
+    public Transform playerBody; // assign this to your 3D cylinder in the Inspector
+
+    public float bodyCrouchScale = 0.5f; // adjust how much you want it to shrink (Y axis)
+    private Vector3 originalBodyScale;
+    private Vector3 originalBodyPosition;
+
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        originalHeight = controller.height;
+        originalCenter = controller.center;
+        originalBodyScale = playerBody.localScale;
+        originalBodyPosition = playerBody.localPosition;
+
+        if (playerCamera != null)
+        {
+            cameraStandPosition = playerCamera.localPosition;
+            cameraCrouchPosition = cameraStandPosition - new Vector3(0, crouchCameraOffset, 0);
+        }
     }
 
     // Update is called once per frame
@@ -38,7 +69,8 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        Vector3 horizontalVelocity = move * speed;
+        float currentSpeed = isCrouching ? crouchSpeed : speed;
+        Vector3 horizontalVelocity = move * currentSpeed;
 
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
@@ -63,5 +95,44 @@ public class PlayerMovement : MonoBehaviour
         }
 
         lastPosition = transform.position;
+
+        //initiate crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            controller.height = crouchHeight;
+
+            float centerOffset = (originalHeight - crouchHeight) / 2f;
+            controller.center = originalCenter - new Vector3(0, centerOffset, 0);
+
+            if (playerBody != null)
+            {
+                Vector3 newScale = new Vector3(originalBodyScale.x, originalBodyScale.y * bodyCrouchScale, originalBodyScale.z);
+                playerBody.localScale = newScale;
+
+                // Move body down by half the amount it shrank
+                float heightDifference = originalBodyScale.y - newScale.y;
+                playerBody.localPosition = originalBodyPosition - new Vector3(0, heightDifference / 2f, 0);
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching = false;
+            controller.height = originalHeight;
+            controller.center = originalCenter;
+
+            if (playerBody != null)
+            {
+                playerBody.localScale = originalBodyScale;
+                playerBody.localPosition = originalBodyPosition;
+            }
+        }
+
+        if (playerCamera != null)
+        {
+            Vector3 targetPosition = isCrouching ? cameraCrouchPosition : cameraStandPosition;
+            playerCamera.localPosition = Vector3.Lerp(playerCamera.localPosition, targetPosition, Time.deltaTime * cameraSmoothSpeed);
+        }
+
     }
 }
