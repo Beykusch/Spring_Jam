d065ca219b87131f35b700759889e529086d41b6
Mehmet Mert Özbaykuş <mehmetozbaykus@gmail.com>
2025-05-12 22:28:37 +0300

    State Machine giriş ve level 2 minifix

--- Assets/EnemyIdleState.cs
+++ Assets/EnemyIdleState.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class EnemyIdleState : StateMachineBehaviour
 {
+    float timer;
+    public float idleTime = 0f;
+
+    Transform player;
+
+    public float detectionAreaRadius = 18f;
+
+    
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
 
