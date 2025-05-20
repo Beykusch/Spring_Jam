98bcdadf07418061281194ec0621a2a9919f01ec
Mehmet Mert Özbaykuş <mehmetozbaykus@gmail.com>
2025-05-20 23:25:35 +0300

    Player Take Damage and Die

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public int HP = 100;
     public HealthBar healthBar;
+
+    public GameObject bloodyScreen;
 
 
     void Start()
@@ -20,10 +25,56 @@ public class Player : MonoBehaviour
         if (HP <= 0)
         {
             print("Player Dead");
+            PlayerDead();
         }
         else
         {
             print("Player Hit");
+            StartCoroutine(BloodyScreenEffect());
+        }
+    }
+
+    private void PlayerDead()
+    {
+        GetComponent<MouseMovement>().enabled = false;
+        GetComponent<PlayerMovement>().enabled = false;
+
+        GetComponentInChildren<Animator>().enabled = true;
+
+    }
+
+    private IEnumerator BloodyScreenEffect()
+    {
+        if(bloodyScreen.activeInHierarchy == false)
+        {
+            bloodyScreen.SetActive(true);
+        }
+
+        var image = bloodyScreen.GetComponentInChildren<Image>();
+
+        Color startColor = image.color;
+        startColor.a = 1f;
+        image.color = startColor;
+
+        float duration = 3f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < duration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+
+            Color newColor = image.color;
+            newColor.a = alpha;
+            image.color = newColor;
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        if (bloodyScreen.activeInHierarchy)
+        {
+            bloodyScreen.SetActive(false);
         }
     }
 
